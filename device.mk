1f812769a304942c98a7650960e6f45537277daa
sm455 <221133194+sm455@users.noreply.github.com>
2026-04-02 11:43:05 +0530

    Revert "Galaga: Correct memtrack hal name"
    This reverts commit 0a60bbd1d0b6792c9d0964cae00d141ce92f54b6.

--- device.mk
+++ device.mk
@@ -110,7 +110,7 @@ PRODUCT_PACKAGES += \
 
 # Graphics
 PRODUCT_PACKAGES += \
-    android.hardware.memtrack-service.mediatek-mali
+    android.hardware.memtrack-service.mediatek
 
 PRODUCT_PACKAGES += \
     ANGLE
