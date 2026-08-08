3c4c3e4dfe09a106854b884b587e6a8f84d1b4e1
Alexander Koskovich <akoskovich@pm.me>
2026-08-08 20:40:38 +0530

    Galaga: Switch to rt_ics vibrator implementation

---
+++ vibrator/ics_haptic_function.h
@@ -0,0 +1,10 @@
+#pragma once
+
+extern "C" {
+extern int sfdc_initialize(void (*callback)(float f0));
+extern int sfdc_calibrate(const int32_t* he, int lenInts);
+extern float sfdc_get_manufactory_f0();
+extern float sfdc_get_continuous_f0();
+extern float sfdc_get_transient_fc();
+extern int sfdc_set_ntc_enable(int enable);
+}
