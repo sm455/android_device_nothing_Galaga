ab65dd760e36c7521882a84f0c39894e748e1e5b
sm455 <221133194+sm455@users.noreply.github.com>
2026-08-12 21:40:31 +0530

    Galaga: Switch to OSS fingerprint

--- device.mk
+++ device.mk
@@ -101,6 +101,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint-service.nothing
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
